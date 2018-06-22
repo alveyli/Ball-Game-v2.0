d237f603a2e23fde779e9ac6a99e9ba2506b2ceb
Alvey Li <li.guangxu.alvey@gmail.com>
2018-06-22 01:04:50 -0600

    The Interface is finished and the buttons are linked with their fountion. Enjoy the game!

---
+++ Assets/Scripts/ToScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToScene : MonoBehaviour {
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("MiniGame");
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+    }
+}
